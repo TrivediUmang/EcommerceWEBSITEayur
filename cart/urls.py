78cf7a5bc81ed33b1803659b656c9eadd005fa26
UmangTrivedi <1000018573@dit.edu.in>
2026-07-10 14:12:39 +0530

    Implemented shopping cart quantity and remove functionality

--- cart/urls.py
+++ cart/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path(
-        "",
-        views.cart_page,
-        name="cart"
-    ),
+    path("", views.cart_view, name="cart"),
 
     path(
         "add/<int:product_id>/",
@@ -15,17 +11,22 @@ urlpatterns = [
         name="add_to_cart"
     ),
 
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.cart_page, name="cart"),
-    path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path(
+        "increase/<int:item_id>/",
+        views.increase_quantity,
+        name="increase_quantity"
+    ),
 
     path(
-    "remove/<int:cart_id>/",
-    views.remove_from_cart,
-    name="remove_from_cart"
-),
+        "decrease/<int:item_id>/",
+        views.decrease_quantity,
+        name="decrease_quantity"
+    ),
+
+    path(
+        "remove/<int:item_id>/",
+        views.remove_from_cart,
+        name="remove_from_cart"
+    ),
+
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,73 +1,89 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
-from .models import Cart
 from products.models import Product
+from .models import Cart, CartItem
 
-
-# -----------------------------
-# Add Product To Cart
-# -----------------------------
 
 @login_required(login_url="login")
 def add_to_cart(request, product_id):
 
     product = get_object_or_404(Product, id=product_id)
 
-    cart_item = Cart.objects.filter(
-        user=request.user,
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
         product=product
-    ).first()
+    )
 
-    if cart_item:
-
+    if not created:
         cart_item.quantity += 1
         cart_item.save()
-
-    else:
-
-        Cart.objects.create(
-            user=request.user,
-            product=product,
-            quantity=1
-        )
 
     return redirect("cart")
 
 
-# -----------------------------
-# Cart Page
-# -----------------------------
-
 @login_required(login_url="login")
-def cart_page(request):
+def cart_view(request):
 
-    cart_items = Cart.objects.filter(user=request.user)
+    cart, created = Cart.objects.get_or_create(user=request.user)
 
-    total = 0
+    items = cart.items.all()
 
-    for item in cart_items:
-        total += item.product.price * item.quantity
+    total = sum(item.total_price for item in items)
 
     context = {
-        "cart_items": cart_items,
-        "total": total
+        "cart": cart,
+        "items": items,
+        "total": total,
     }
 
     return render(request, "cart.html", context)
 
-
-# -----------------------------
-# Remove Product
-# -----------------------------
-
 @login_required(login_url="login")
-def remove_from_cart(request, cart_id):
+def increase_quantity(request, item_id):
 
     item = get_object_or_404(
-        Cart,
-        id=cart_id,
-        user=request.user
+        CartItem,
+        id=item_id,
+        cart__user=request.user
+    )
+
+    item.quantity += 1
+    item.save()
+
+    return redirect("cart")
+
+
+@login_required(login_url="login")
+def decrease_quantity(request, item_id):
+
+    item = get_object_or_404(
+        CartItem,
+        id=item_id,
+        cart__user=request.user
+    )
+
+    if item.quantity > 1:
+
+        item.quantity -= 1
+
+        item.save()
+
+    else:
+
+        item.delete()
+
+    return redirect("cart")
+
+
+@login_required(login_url="login")
+def remove_from_cart(request, item_id):
+
+    item = get_object_or_404(
+        CartItem,
+        id=item_id,
+        cart__user=request.user
     )
 
     item.delete()
