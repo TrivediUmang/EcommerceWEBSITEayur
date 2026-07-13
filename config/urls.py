0c509b8b718febd4d0fb70e86f41769b7d5aeee5
UmangTrivedi <1000018573@dit.edu.in>
2026-07-13 12:26:20 +0530

    orders update

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,9 @@
 """
 URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,21 +11,35 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
 
+    # Admin
+    path("admin/", admin.site.urls),
+
+    # Django Allauth
     path("accounts/", include("allauth.urls")),
 
-    # Products App
-    path('', include('products.urls')),
+    # Products (Home, Shop, About, Contact, Blogs)
+    path("", include("products.urls")),
 
-    # Cart App
-    path('cart/', include('cart.urls')),
-  
-     path("account/", include("accounts.urls")),
+    # Cart
+    path("cart/", include("cart.urls")),
+
+    # User Dashboard
+    path("account/", include("accounts.urls")),
+
+    # Orders
+    path("orders/", include("orders.urls")),
+
 ]
 
-# Mendatory
-urlpatterns += static(settings.MEDIA_URL, 
-    document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, 
-    document_root=settings.STATIC_ROOT)
+# Serve Media & Static Files (Development)
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Order
 
-# Create your views here.
+@login_required
+def my_orders(request):
+
+    orders = Order.objects.filter(user=request.user).order_by("-ordered_at")
+
+    return render(request, "orders/orders.html", {
+        "orders": orders
+    })
