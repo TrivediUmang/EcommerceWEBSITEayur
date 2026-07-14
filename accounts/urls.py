90e9b519046fc8956e8fc0c22ab73debedf9f451
UmangTrivedi <1000018573@dit.edu.in>
2026-07-14 12:35:51 +0530

    all

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # Dashboard
     path("", views.profile, name="profile"),
+
+    # Saved Address
+    path(
+        "addresses/",
+        views.saved_addresses,
+        name="saved_addresses"
+    ),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 @login_required(login_url="login")
 def profile(request):
     return render(request, "accounts/profile.html")
+
+    from django.contrib.auth.decorators import login_required
+
+@login_required
+def saved_addresses(request):
+
+    addresses = request.user.addresses.all()
+
+    return render(
+        request,
+        "accounts/addresses.html",
+        {
+            "addresses": addresses
+        }
+    )
