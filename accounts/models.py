eedaf8b07c32c0b79b0f631f2ed1d8ba7d793206
UmangTrivedi <1000018573@dit.edu.in>
2026-07-14 12:23:04 +0530

    models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Address(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="addresses"
+    )
+
+    full_name = models.CharField(max_length=150)
+
+    phone = models.CharField(max_length=15)
+
+    house = models.CharField(max_length=255)
+
+    area = models.CharField(max_length=255)
+
+    city = models.CharField(max_length=100)
+
+    state = models.CharField(max_length=100)
+
+    pincode = models.CharField(max_length=10)
+
+    country = models.CharField(
+        max_length=100,
+        default="India"
+    )
+
+    is_default = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return f"{self.full_name} - {self.city}"
